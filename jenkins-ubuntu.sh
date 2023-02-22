45dc532fca52f7652b508c71bd53b80ca5b3553c
devopjulien <oubdajulien@yahoo.fr>
2023-02-22 04:35:02 -0500

    script modified

--- jenkins-ubuntu.sh
+++ jenkins-ubuntu.sh
@@ -5,14 +5,13 @@
 #Description: Installation of jenkins on ubuntu
 
 
-# Update system
+# Update the systeme
 sudo apt-get update
 ##Install java
-sudo apt-get install openjdk-11-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo apt-get install openjdk-11-jdk
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
 sudo apt-get update
 sudo apt-get install jenkins -y
 sudo apt install git -y
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
-  
